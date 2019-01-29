3e683f10a6b0c1a8bfe24a15356d0c247fcf78ca
Jeffrey Yu <jwy5140@gmail.com>
2019-01-29 05:35:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function shout(string)
 {
   var string2 = string.split();
-  for (n = 0; n < string.length; n++)
+  for (var n = 0; n < string.length; n++)
   {
     string2[n] = string2[n] - 32;
   }
