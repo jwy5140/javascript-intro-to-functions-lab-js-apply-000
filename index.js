359c3ff9bd353d6512e3c226648cf4676666c8c5
Jeffrey Yu <jwy5140@gmail.com>
2019-01-29 05:41:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function whisper(string)
   var string2 = string.toLowerCase();
   return string2;
 }
+
+function logShout(string)
+{
+  var string2 = string.toUpperCase();
+  console.log(string2);
+}
+
+function logWhisper(string)
+{
+  var string2 = string.toLowerCase();
+  console.log(string2);
+}
